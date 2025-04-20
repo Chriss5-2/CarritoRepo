9843e2b3bc0986176b4921665c8fb454a72fe98c
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-20 16:07:24 -0500

    Modify carrito.py

--- src/carrito.py
+++ src/carrito.py
@@ -100,5 +100,13 @@ class Carrito:
         """
         Vacía la lista de productos
         """
-        self.items.clear()
+        self.items = []
         return self.items
+
+#   def aplicar_descuento_condicionar(porcentaje, mínimo):
+        """
+        Aplica descuento determinado solo si el monto total es mayor al mínimo dado
+        """
+#        if self.calcular_total() >= mínimo:
+#            return self.aplicar_descuento(porcentaje=porcentaje)
+#        return self.calcular_total()
