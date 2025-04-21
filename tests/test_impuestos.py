053178374a52afdc2fe528faefab775d536811df
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-21 01:11:54 -0500

    Solve problem with files ubication

--- tests/test_impuestos.py
+++ tests/test_impuestos.py
@@ -3,17 +3,26 @@ import pytest
 from src.carrito import Carrito
 from src.factories import ProductoFactory
 
-def test_calcular_impuestos():
+# REFRACTOR
+@pytest.mark.parametrize("prod1, porcentaje, cumple", [
+    ("producto_smartwatch", 10, True),
+    ("producto_smartwatch", 110, False),
+    ("producto_smartwatch", -5, False)
+])
+def test_calcular_impuestos(carrito, prod1, porcentaje, cumple, request):
     """
     Red: Se espera que calcular_impuestos retorne el valor del impuesto.
     """
     # Arrange
-    carrito = Carrito()
-    producto = ProductoFactory(nombre="Producto", precio=250.00)
+    producto = request.getfixturevalue(prod1)
+
     carrito.agregar_producto(producto, cantidad=4)  # Total = 1000
 
-    # Act
-    impuesto = carrito.calcular_impuestos(10)  # 10% de 1000 = 100
-
-    # Assert
-    assert impuesto == 100.00
+    # Act & Assert
+    if cumple:
+        impuesto = carrito.calcular_impuestos(porcentaje)  # 10% de 1000 = 100
+        total_impuesto = carrito.calcular_total()*porcentaje/100 # 100
+        assert impuesto == total_impuesto
+    else:
+        with pytest.raises(ValueError):
+            carrito.calcular_impuestos(porcentaje)
